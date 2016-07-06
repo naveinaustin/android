afe3fae01cea9c91f9540f32bdc2e73ed57263d1
Navein Austin Fernandes <naveinaustin@gmail.com>
2016-07-06 12:23:10 +0530

    Starbuzz App from Head First Android

---
+++ Starbuzz/app/src/main/java/org/example/starbuzz/DrinkActivity.java
@@ -0,0 +1,35 @@
+package org.example.starbuzz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DrinkActivity extends AppCompatActivity {
+
+    public static final String EXTRA_DRINKNO = "EXTRA_DRINKNO";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        int drinkNo = (Integer) getIntent().getExtras().get(EXTRA_DRINKNO);
+        Drink drink = Drink.drinks[drinkNo];
+
+        ImageView photo = (ImageView)findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
+
+
+        TextView name = (TextView)findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        TextView description = (TextView)findViewById(R.id.description);
+        description.setText(drink.getDescription());
+    }
+
+}
